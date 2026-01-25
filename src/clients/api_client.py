1ee64bad7e8bb08a8d98f32eaa2298a98484d115
Wiktor B <viktorbychinskij@gmail.com>
2026-01-26 00:45:25 +0100

    api client -static agnostic now

--- src/clients/api_client.py
+++ src/clients/api_client.py
@@ -18,30 +18,27 @@ class ApiClient:
     def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
         self.client.close()
 
-    def get(self, endpoint: str, params: Optional[Dict[str, Any]] = None) -> Dict[str, Any]:
+    def get(self, endpoint: str, params: Optional[Dict[str, Any]] = None) -> httpx.Response:
         self.logger.info(f"Making GET request to {endpoint} with params {params}...")
         url = urljoin(self.base_url, endpoint)
-        response = self.client.get(url, params=params)
-        response.raise_for_status()
-        return response.json()
+        return self.client.get(url, params=params)
 
-    def post(self, endpoint: str, data: Optional[Dict[str, Any]] = None) -> Dict[str, Any]:
+    def post(self, endpoint: str, data: Optional[str | Dict[str, Any]] = None) -> httpx.Response:
         self.logger.info(f"Making POST request to {endpoint} with data {data}...")
         url = urljoin(self.base_url, endpoint)
-        response = self.client.post(url, json=data)
-        response.raise_for_status()
-        return response.json()
-
-    def put(self, endpoint: str, data: Optional[Dict[str, Any]] = None) -> Dict[str, Any]:
+        if isinstance(data, str):
+            return self.client.post(url, data=data)
+        return self.client.post(url, json=data)
+     
+    def put(self, endpoint: str, data: Optional[Dict[str, Any]] = None) -> httpx.Response:
         self.logger.info(f"Making PUT request to {endpoint} with data {data}...")
         url = urljoin(self.base_url, endpoint)
-        response = self.client.put(url, json=data)
-        response.raise_for_status()
-        return response.json()
+        return self.client.put(url, json=data)
 
     def set_auth_token(self, token: str) -> None:
         self._auth_token = token
-        self.client.headers = self.get_headers()
+        self.client.close()
+        self.client = httpx.Client(headers=self.get_headers())
 
     def get_headers(self) -> Dict[str, str]:
         # TODO: Refactor to avoid code duplication, add support for additional headers/tokens
@@ -54,3 +51,10 @@ class ApiClient:
             "Authorization": f"Bearer {self._auth_token}",
             "Content-Type": "application/json"
         }
+        
+    def clone(self) -> ApiClient:
+        return ApiClient(
+            base_url=self.base_url,
+            logger=self.logger,
+            auth_token=self._auth_token
+    )
